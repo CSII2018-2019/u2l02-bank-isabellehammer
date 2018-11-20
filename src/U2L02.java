a617e7cc18e5d5dfa6353daaf660b5f849df40c6
Isabelle <hammeri@theellisschool.org>
2018-11-19 21:43:49 -0500

    not sure I understand the part where I convert the balance to a string

--- src/U2L02.java
+++ src/U2L02.java
@@ -1,4 +1,42 @@
 
 public class U2L02 {
+	private String userName;
+	private int currentBalance;
+	
+	public U2L02 () {
+		setName("Not Set");
+		setBalance(0);
+	}
+	
+	private void setName(String string) {
+		// TODO Auto-generated method stub
+		
+	}
 
+	public U2L02(String n, int balance, int low) {
+		setName(n);
+		setBalance(balance);
+	}
+	
+	public String getName() {
+		return userName;
+	}
+	
+	public int getBalance() {
+		return currentBalance;
+	}
+	
+	public void setBalance(int balance) {
+		if (balance >= 100) {
+			currentBalance = balance;
+		}else {
+			currentBalance = 100;
+			System.out.println("The lowest balance allowable is $100. Defaulting to 100.");
+		}
+	}
+	
+	public String toString() {
+		return getName() + ", you have $" + getBalance() + " in your account.";
+	}
+	
 }
