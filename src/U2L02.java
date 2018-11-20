43115f2eec63bf5805905cde0176c2fc32b72517
Isabelle <hammeri@theellisschool.org>
2018-11-20 09:08:00 -0500

    ok I added some things and now I'm kind of confused

--- src/U2L02.java
+++ src/U2L02.java
@@ -4,11 +4,13 @@ public class U2L02 {
 	private String userName;
 	private int currentBalance;
 	private int lowestBalanceAllowed;
+	private int withdrawal;
 	
 	public U2L02 () {
 		setName("Not Set");
 		setBalance(0);
 		setLowest(100);
+		setWithdrawal(0);
 	}
 	
 	private void setName(String string) {
@@ -16,23 +18,36 @@ public class U2L02 {
 		
 	}
 
-	public U2L02(String n, int balance, int low) {
+	public U2L02(String n, int balance, int low, int w) {
 		setName(n);
 		setBalance(balance);
 		setLowest(low);
+		setWithdrawal(w);
 	}
 	
+	
+	private void setLowest(int low) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void setWithdrawal(int w) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	public String getName() {
 		return userName;
 	}
 	
 	public int getBalance() {
-		return currentBalance;
+		return (currentBalance - withdrawal);
 	}
 	
 	public int getLowest() {
 		return lowestBalanceAllowed;
 	}
+	
 	
 	public void setBalance(int balance) {
 		if (balance >= lowestBalanceAllowed) {
